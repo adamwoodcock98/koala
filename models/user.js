cae6ba7309e40224d809c5a5ac4696f112c93c74
Rob Oman <rob.oman@icloud.com>
2022-04-14 10:51:08 +0100

    Add User Schema and validation functions

--- models/user.js
+++ models/user.js
@@ -1,10 +1,41 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const UserSchema = new mongoose.Schema({
-  email: String,
-  password: String,
+  firstName: {
+    type: String,
+    // required: true
+  },
+  lastName: {
+    type: String,
+    // required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [
+      { validator: validator.isEmail, message: 'Invalid Email' },
+      { validator: async function(email) {
+        const user = await this.constructor.findOne({ email });
+        return !!!user;
+      }, message: 'Email already exists' }
+    ]
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  posts: {
+    type: [mongoose.SchemaTypes.ObjectId]
+  }
 });
 
 const User = mongoose.model("User", UserSchema);
+
+// async function isUnique(email) {
+//   const user = await UserSchema.findOne( { email });
+//   return !!user;
+// }
+
 
 module.exports = User;
