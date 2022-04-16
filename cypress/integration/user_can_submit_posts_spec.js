454dfdf8219b857f710f4aa1c4d4171936e0a70f
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-16 13:12:37 +0100

    Fixed failing tests

--- cypress/integration/user_can_submit_posts_spec.js
+++ cypress/integration/user_can_submit_posts_spec.js
@@ -1,7 +1,7 @@
 describe("Timeline", () => {
   it("can submit posts, when signed in, and view them", () => {
-    cy.signUp;
-    cy.login;
+    cy.signUp();
+    cy.login();
     
     cy.addPost('Hello, world!');
 
