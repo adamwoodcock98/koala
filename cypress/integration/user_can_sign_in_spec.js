5ef8d3199dc3af5c7d351f7f3e1f64350baec4fd
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-16 12:53:21 +0100

    Updated Schema Helper
    Added defaults to schema helpers
    
    Refactored integration tests to DRY the code.

--- cypress/integration/user_can_sign_in_spec.js
+++ cypress/integration/user_can_sign_in_spec.js
@@ -1,12 +1,7 @@
 describe("Authentication", () => {
   it("A user signs in and is redirected to /posts", () => {
-    const firstName = "Barry";
-    const lastName = "Barry";
-    const email = "test@example.com";
-    const password = "12345";
-
-    cy.signUp(firstName, lastName, email, password);
-    cy.login(email, password);
+    cy.signUp;
+    cy.login;
 
     cy.url().should("include", "/posts");
   });
