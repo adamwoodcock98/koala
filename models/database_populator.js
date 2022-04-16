6af5c3e835e85e85ad2f161c7ad7e0f02ea7af66
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-16 18:46:38 +0100

    Updated populator
    Helper now populates a friends array and an about me section.
    
    Extracted about me into a separate file

--- models/database_populator.js
+++ models/database_populator.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Post = require("./post");
 const User = require("./user");
+const AboutMe = require("./about_me.js");
 
 mongoose.connect("mongodb://127.0.0.1/acebook", {
   useNewUrlParser: true,
@@ -11,6 +12,15 @@ const db = mongoose.connection;
 
 console.log(db.name);
 
+const barrysFriends = [
+  mongoose.Types.ObjectId("62596c9539f4ca6d024cee07"),
+  mongoose.Types.ObjectId("62596cc70b90a16ceeaf66f1"),
+  mongoose.Types.ObjectId("62596cd10b90a16ceeaf66fa"),
+  mongoose.Types.ObjectId("625979737890bb083ccb43bf"),
+  mongoose.Types.ObjectId("6259797f7890bb083ccb43c8"),
+  mongoose.Types.ObjectId("62597a290f8ad516167bcd4d"),
+];
+
 const users = [
   {
     _id: "62596c9539f4ca6d024cee07",
@@ -18,6 +28,7 @@ const users = [
     firstName: "Larry",
     lastName: "Larrysson",
     profilePicture: "/images/users/larry_larryson.jpg",
+    aboutMe: "625afd523b344e8aab81d05b",
   },
   {
     _id: "62596cc70b90a16ceeaf66f1",
@@ -25,6 +36,7 @@ const users = [
     firstName: "Carrie",
     lastName: "Carriesdottir",
     profilePicture: "/images/users/carrie_carriedottir.jpg",
+    aboutMe: "625afd523b344e8aab81d05b",
   },
   {
     _id: "62596cd10b90a16ceeaf66fa",
@@ -32,6 +44,7 @@ const users = [
     firstName: "Gary",
     lastName: "Garysson",
     profilePicture: "/images/users/gary_garyson.jpg",
+    aboutMe: "625afd523b344e8aab81d05b",
   },
   {
     _id: "625979737890bb083ccb43bf",
@@ -39,6 +52,7 @@ const users = [
     firstName: "Liam",
     lastName: "Liamsson",
     profilePicture: "/images/users/liam_liamson.jpg",
+    aboutMe: "625afd523b344e8aab81d05b",
   },
   {
     _id: "6259797f7890bb083ccb43c8",
@@ -46,6 +60,7 @@ const users = [
     firstName: "Monica",
     lastName: "Monicasdottir",
     profilePicture: "/images/users/monica_monicadottir.jpg",
+    aboutMe: "625afd523b344e8aab81d05b",
   },
   {
     _id: "62597a290f8ad516167bcd4d",
@@ -53,7 +68,17 @@ const users = [
     firstName: "Barold",
     lastName: "Baroldsson",
     profilePicture: "/images/users/barold_baroldson.jpg",
+    aboutMe: "625afd523b344e8aab81d05b",
   },
+  {
+    _id: "625acd9cc936214661cf5692",
+    email: "testx@example.com",
+    firstName: "Barry",
+    lastName: "Barry-Barroldsson",
+    profilePicture: "/images/users/barry_barry-barroldsson.jpg",
+    friends: barrysFriends,
+    aboutMe: "625afd523b344e8aab81d05b",
+  }
 ];
 
 // INFO: This will currently throw an error if there is already a user with the email in the database
@@ -64,15 +89,29 @@ users.forEach((user) => {
       _id: user._id,
       email: user.email,
       firstName: user.firstName,
-      friends: [],
+      friends: user.friends || [],
       lastName: user.lastName,
       password: "12345",
       profilePicture: user.profilePicture,
+      aboutMe: user.aboutMe,
     });
   } catch (e) {
     console.log("Caught Error:", e);
   }
 });
+
+// Create an about me
+  try {
+    AboutMe.create({
+      education: "Barry Grammar School for Barry's",
+      workplace: "MarryBarry Dating Service",
+      relationshipStatus: "It's Complicated",
+      pronouns: "They/Them",
+    });
+  } catch (e) {
+    console.log("Caught Error:", e);
+  }
+
 
 const messages = [
   "we iz so confused",
