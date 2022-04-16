8a21e6f7bc88d3d4b8b8051320e7716338458736
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-16 18:58:33 +0100

    Extracted AboutMe Tests
    Created a new test file, extracted the tests and split them into individual assertions

---
+++ spec/models/about_me.spec.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+
+require("../mongodb_helper");
+const AboutMe = require("../../models/about_me.js");
+
+describe("AboutMe model", () => {
+  beforeEach((done) => {
+    mongoose.connection.collections.aboutmes.drop(() => {
+      done();
+    });
+  });
+
+
+  it("has an education me value", () => {
+    const aboutMe = new AboutMe({
+      education: "Barry Grammar for Barrys",
+    })
+    expect(aboutMe.education).toBe("Barry Grammar for Barrys");
+  });
+
+  it("has a workplace value", () => {
+    const aboutMe = new AboutMe({
+      workplace: "Barry X",
+    })
+    expect(aboutMe.workplace).toBe("Barry X");
+  });
+
+  it("has an about me section", () => {
+    const aboutMe = new AboutMe({
+      relationshipStatus: "In a relationship with Barry",
+    })
+    expect(aboutMe.relationshipStatus).toBe("In a relationship with Barry");
+  });
+
+  it("has an about me section", () => {
+    const aboutMe = new AboutMe({
+      pronouns: "They/Them"
+    })
+    expect(aboutMe.pronouns).toBe("They/Them");
+  });
+
+});
