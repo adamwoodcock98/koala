9ba5052b9b7166d4aa881fb14d97b326a8258397
clovellbsc <clovellbsc@gmail.com>
2022-04-16 18:30:15 +0100

    implemented logging out feature and associated tests run and passing

---
+++ cypress/integration/user_can_log_out_spec.js
@@ -0,0 +1,27 @@
+describe("Logging Out", () => {
+  const firstName = "Barry";
+  const lastName = "Barry";
+  const email = "test@example.com";
+  const password = "12345";
+
+  it("A user that logs out is redirected to /sessions", () => {
+    cy.signUp(firstName, lastName, email, password);
+    cy.login(email, password);
+    cy.get("#dropdownUser1").click();
+    cy.get("#signOut").click();
+
+    cy.url().should("not.include", "/posts");
+    cy.url().should("include", "/sessions");
+  });
+
+  it("A user that has logged out cannot view posts", () => {
+    cy.signUp(firstName, lastName, email, password);
+    cy.login(email, password);
+    cy.get("#dropdownUser1").click();
+    cy.get("#signOut").click();
+    cy.visit("/posts");
+
+    cy.url().should("not.include", "/posts");
+    cy.url().should("include", "/sessions");
+  });
+});
