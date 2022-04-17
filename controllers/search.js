269e560b0be156982c8ae90abd374b9da26c9570
clovellbsc <clovellbsc@gmail.com>
2022-04-17 09:43:12 +0100

    Search returns unique array of user objects and passes them to handlebars. Functionality to direct search through search bar yet to be added

--- controllers/search.js
+++ controllers/search.js
@@ -2,7 +2,7 @@ const User = require("../models/user");
 
 const SearchController = {
   Index: (req, res) => {
-    const searchArray = ["Bar", "Barry", "bar"];
+    const searchArray = req.body.message.split(/[ ,]+/);
 
     const users = [];
     searchArray.forEach((name) => {
@@ -11,19 +11,21 @@ const SearchController = {
         lastName: { $regex: name, $options: "i" },
       }).then((user) => {
         users.push(user);
-        console.log("This is just one user return", user);
-        console.log("Pre reduce method", users);
+        const merged = [].concat.apply([], users);
+        console.log("Merged------>", merged);
+        const uniqueArray = merged.filter((value, index) => {
+          const _value = JSON.stringify(value);
+          return (
+            index ===
+            merged.findIndex((obj) => {
+              return JSON.stringify(obj) === _value;
+            })
+          );
+        });
+        res.render("search/index", { users: uniqueArray });
       });
     });
-    users.reduce((acc, val) => acc.concat(val), []);
-    removeDuplicates(users);
-    console.log("Post reduce method", users);
-    res.render("search/index", { users: users });
   },
 };
-
-function removeDuplicates(arr) {
-  return arr.filter((item, index) => arr.indexOf(item) === index);
-}
 
 module.exports = SearchController;
