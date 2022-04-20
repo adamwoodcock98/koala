7f58e126ca957d2cc0f3ace74584b77aa7af789f
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-20 19:42:51 +0100

    Remove friend
    Test drove functionality to remove a friend once added.
    
    Co-authored-by: Rob Oman <r94o@users.noreply.github.com>

--- cypress/integration/user_can_view_profile_spec.js
+++ cypress/integration/user_can_view_profile_spec.js
@@ -1,5 +1,4 @@
 describe.only("Viewing a profile", () => {
-  
   it("should displays the users details", () => {
     cy.signUp();
     cy.login();
@@ -7,13 +6,13 @@ describe.only("Viewing a profile", () => {
     cy.get("#dropdown-profile-link").click();
 
     cy.get(".friends-container").should("contain", "Your friends");
-    cy.get(".posts-container").should("contain", "Shared by you")
+    cy.get(".posts-container").should("contain", "Shared by you");
 
-    cy.get(".about-container").should("contain", "About me")
+    cy.get(".about-container").should("contain", "About me");
   });
 
   it("should dynamically change the profile labelling", () => {
-    cy.signUp("Harry", "Harry-Harrisson", "harry@example.com", "h4rry")
+    cy.signUp("Harry", "Harry-Harrisson", "harry@example.com", "h4rry");
     cy.signUp();
     cy.login();
 
@@ -23,7 +22,6 @@ describe.only("Viewing a profile", () => {
 
     cy.get(".friends-container").should("contain", "Harry's friends");
     cy.get(".posts-container").should("contain", "Shared by Harry");
-
   });
 
   it("should display the users posts", () => {
@@ -48,7 +46,7 @@ describe.only("Viewing a profile", () => {
 
     cy.get("#dropdownUser1").click();
     cy.get("#dropdown-profile-link").click();
-    cy.get("#add-friend-button").should("not.exist");
+    cy.get("#friend-unfriend-button").should("not.exist");
 
     cy.get("#dropdownUser1").click();
     cy.get("#sign-out-button").click();
@@ -59,7 +57,27 @@ describe.only("Viewing a profile", () => {
     cy.get("#searchButton").click();
     cy.get(".user-container").click();
 
-    cy.get("#add-friend-button").should("exist");
+    cy.get("#friend-unfriend-button").should("exist");
+  });
+
+  it("should have a remove friend button if already friends", () => {
+    cy.signUp("Rick", "Rick-Rickinsson", "rick@ick.com", "securer1ck");
+
+    cy.signUp();
+    cy.login();
+
+    cy.get("#searchBox").type("Rick Rick-Rickinsson");
+    cy.get("#searchButton").click();
+    cy.get(".user-container").click();
+
+    cy.get("#friend-unfriend-button").click(); //Now they're a friend
+
+    cy.get("#friend-unfriend-button").should("contain", "Unfriend");
+    cy.get("#friend-unfriend-button").click();
+    
+    
+    cy.get("#friend-unfriend-button").should("contain", "Add friend");
+    cy.get("#friend-name").should("not.exist");
   });
 
   it("should add a friend and list the friend on the users profile", () => {
@@ -72,7 +90,7 @@ describe.only("Viewing a profile", () => {
     cy.get("#searchButton").click();
     cy.get(".user-container").click();
 
-    cy.get("#add-friend-button").click();
+    cy.get("#friend-unfriend-button").click();
 
     cy.get(".friend-name").should("contain", "Barry Barry-Barroldsson");
 
@@ -80,8 +98,5 @@ describe.only("Viewing a profile", () => {
     cy.get("#dropdown-profile-link").click();
 
     cy.get(".friend-name").should("contain", "Rick Rick-Rickinsson");
-  })
-
+  });
 });
-
-
