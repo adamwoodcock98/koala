bf647adb23cc81b6efb6b0e87399c14b96dc70ac
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-21 17:47:37 +0100

    Added functionality to edit profile

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,31 +1,55 @@
 // const User = require("../models/user");
 const Post = require("../models/post");
 const User = require("../models/user");
-require("../models/about_me");
+const AboutMe = require("../models/about_me");
 
 const ProfileController = {
   Index: (req, res) => {
-    const userID = req.params.userID
+    const userID = req.params.userID;
 
     User.findById(userID)
       .populate("aboutMe")
       .populate("friends")
-      .then(user => {
+      .then((user) => {
         Post.find({ user: user._id }, (err, posts) => {
           if (err) {
             console.log(err);
           }
           user.posts = posts;
-          user.isProfileOwner = (userID === req.session.user._id);
+          user.isProfileOwner = userID === req.session.user._id;
 
-          const friendIDArray = user.friends.map(user => {
+          const friendIDArray = user.friends.map((user) => {
             return user._id;
-          })
-          
+          });
+
+          if (req.originalUrl.includes("edit")) {
+            user.isEditing = "";
+          } else {
+            user.isEditing = "disabled";
+          }
+
           user.isFriends = friendIDArray.includes(req.session.user._id);
-          
-          res.render("profile/index", { user: user, loggedInUserId: req.session.user._id });
+
+          res.render("profile/index", {
+            user: user,
+            loggedInUserId: req.session.user._id,
+          });
         });
+      });
+  },
+  Update: (req, res) => {
+    const data = req.body;
+
+    console.log("THE ABOUT ME: ", req.session.user.aboutMe);
+
+    AboutMe.findById(req.session.user.aboutMe)
+      .then((aboutMe) => {
+        aboutMe.education = data.education;
+        aboutMe.workplace = data.workplace;
+        aboutMe.relationshipStatus = data.relationshipStatus;
+        aboutMe.save().then(() => {
+          res.redirect(`/profile/${req.session.user._id}`);
+        })
       });
   },
 };
--- routes/profile.js
+++ routes/profile.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const ProfileController = require("../controllers/profile");
 
 router.get("/:userID", ProfileController.Index);
+router.get("/:userID/edit", ProfileController.Index);
+router.post("/:userID/edit", ProfileController.Update);
 
 module.exports = router;
