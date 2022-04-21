d7646eab03217b8e71c83a5c0bd1fed291bde9e2
Rob Oman <rob.oman@icloud.com>
2022-04-21 15:56:42 +0100

    user can delete see and delete notifications

--- routes/notifications.js
+++ routes/notifications.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const NotificationsController = require("../controllers/notifications");
 
 router.get("/", NotificationsController.Index);
+router.delete("/delete/:notificationID", NotificationsController.Delete);
 
 module.exports = router;
