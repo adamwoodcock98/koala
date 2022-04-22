ae0665678405d7cc85bd920ef27ea258da87aab6
Adam Woodcock <42064260+adamwoodcock98@users.noreply.github.com>
2022-04-22 12:36:52 +0100

    Removed unused assets

--- routes/post_comments 2.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PostCommentsController = require("../controllers/post_comments");
-
-router.post("/new", PostCommentsController.Create);
-
-module.exports = router;
